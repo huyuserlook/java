a6c5829414acaff33abd1e72d7c1cb2aa9be39ed
huyuserlook <153735194+huyuserlook@users.noreply.github.com>
2026-01-20 10:32:23 +0700

    init

---
+++ src/hoidanitdemo/HelloWorld.java
@@ -0,0 +1,11 @@
+package hoidanitdemo;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world with java and hoi dân it");
+		System.out.println("hello world with java and hoi dân it");
+		System.out.println("hello world with java and hoi dân it");
+
+	}
+
+}
